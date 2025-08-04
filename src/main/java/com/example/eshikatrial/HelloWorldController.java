aeb12899d95d6f4ce5bffb79729ed51ec8e3a8ca
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-04 16:06:08 +0530

    Initial commit from ServiceNow

---
+++ src/main/java/com/example/eshikatrial/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.eshikatrial;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    private final HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return helloWorldService.getHelloWorld();
+    }
+}
